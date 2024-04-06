cf457d4a6f7f0cf85174989d0fe8d37d8e3bffb9
Tatsiana <badpanda@tut.by>
2024-04-06 20:44:30 +0300

    Lesson 1: movie db from user by prompt function. Now with the script)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log(1);
+"use strict";
+const numberOfFilms = 5;
+
+let personalMovieDB = {
+    'count' : numberOfFilms,
+    'movies' : {},
+    'actors' : {},
+    'genres' : [],
+    'privat' : false
+};
+
+let movieName = prompt("Один из последних просмотренных фильмов?", '');
+let movieRating = +prompt("На сколько оцените его?", "5");
+
+personalMovieDB.movies[movieName] = movieRating;
+
+movieName = prompt("Один из последних просмотренных фильмов?", "");
+movieRating = +prompt("На сколько оцените его?", "5");
+personalMovieDB.movies[movieName] = movieRating;
+
+console.log(personalMovieDB.movies);
